c19ea65472925ea9c5b68d1d6723cb2bd973500b
Sharon0320 <sharon0320@gachon.ac.kr>
2024-04-05 03:45:52 +0900

    첫화면 2차 수정 및 아이콘 5초 눌렀을 때 관리자 화면 연동

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import {PaymentChoice} from "./paymentChoice/PaymentChoice";
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FirstPage from './FirstPage';
+import SalesStatistics from './SalesStatistics';
 
 function App() {
   return (
-      <>
-      </>
-    // <Router>
-    //   <PaymentChoice />
-    // </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FirstPage />} />
+        <Route path="/salesStatistics" element={<SalesStatistics />} />
+      </Routes>
+    </Router>
   );
 }
 
