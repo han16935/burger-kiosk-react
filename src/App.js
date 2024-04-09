596965746b3b107233a178938c9f8b28a30cafdf
Sharon0320 <sharon0320@gachon.ac.kr>
2024-04-09 16:28:37 +0900

    화면이동 2차

--- src/App.js
+++ src/App.js
@@ -14,8 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<FirstPage/>}/>
           <Route path="/salesStatistics" element={<SalesStatistics/>}/>
-          <Route path="/SecondPage" element={<SecondPage/>}/>
-          <Route path="/ShoppingBag" element={<ShoppingBag/>}/>
+          <Route path="/SecondPage" element={<SecondPage/>}>
+            <Route path="ShoppingBag" element={<ShoppingBag/>}/>
+          </Route>
           <Route path="/PaymentChoice" element={<PaymentChoice/>}/>
         </Routes>
       </Router>
