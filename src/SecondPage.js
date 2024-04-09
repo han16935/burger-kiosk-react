fa51d898e500f476116b395b5c15eedbae3fbac1
Yim202035379 <yim0619@gachon.ac.kr>
2024-04-09 22:24:19 +0900

    뒤로가기 이미지 추가, 기능 구현

--- src/SecondPage.js
+++ src/SecondPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./SecondPage.css"; // CSS 스타일을 적용하기 위해 임포트합니다.
+import {useNavigate} from "react-router-dom"//뒤로가기 기능 구현용
 import ShoppingBag from "./ShoppingBag"; // ShoppingBag 컴포넌트를 임포트합니다.
 import whopper from './img/guinness_whopper.jpg';
 import BLT from './img/chickenkingBLT.jpg';
@@ -7,6 +8,8 @@ import cola from './img/cocacola.jpg';
 import fanta from './img/fanta.jpg';
 import frenchfries from './img/frenchfries.jpg';
 import cheesestick from './img/cheesestick.jpg';
+import back from './img/back.png';
+
 const menu = [
     { id: 1, name: 'Guinness Whopper', price: '5000', image: whopper },
     { id: 2, name: 'ChickenKing BLT', price: '4500', image: BLT },
@@ -17,6 +20,9 @@ const menu = [
 ];
 
 export function SecondPage() {
+
+    const navigate = useNavigate();
+
     const [menuCounts, setMenuCounts] = useState(menu.map(menuItem => ({ ...menuItem, count: 0 })));
 
     const handleMenuClick = (id) => {
@@ -54,7 +60,7 @@ export function SecondPage() {
     return (
         <div>
             <header>
-                <button style={{width:'100px', height:'100px'}}><img src="/img/back.png" style={{width:'60px', height:'60px'}}></img></button> {/* 이 버튼이 실제로 동작하도록 하려면 추가적인 로직이 필요합니다. */}
+                <button onClick={()=> navigate(-1)} style={{width:'100px', height:'100px'}}><img src= {back} style={{width:'60px', height:'60px'}}></img></button> {/* 이 버튼이 실제로 동작하도록 하려면 추가적인 로직이 필요합니다. */}
             </header>
             <ul>{menuItems}</ul>
             <ShoppingBag items={menuCounts.filter(menuItem => menuItem.count > 0)} onCountChange={handleCountChange} totalPrice={totalPrice} />
